cba049edf5d06a9dccd1d5308c3f64eb50cde470
Makotox0207 <xiaoxishicheng@gmail.com>
2021-08-11 00:22:34 +0900

    sorceryでログイン機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :success, :info, :warning, :danger
+  # :require_login sorceryが作成するメソッド。ログインしてない時not_authenticatedメソッドを発火する
+  before_action :require_login
+  def not_authenticated
+    redirect_to login_url, danger: 'ログインしてください'
+  end
 end
